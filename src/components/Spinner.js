ba874f966e6d4c77ad4b82875ef20889d7d66dd0
rj-freaker <68267959+rj-freaker@users.noreply.github.com>
2023-10-07 10:06:55 +0530

    Create Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../App.css'
+export default function Spinner() {
+  return (
+    <div className='flex w-full h-full justify-center items-center'>
+      <div className='loader items-center justify-center'></div>
+    </div>
+  )
+}
